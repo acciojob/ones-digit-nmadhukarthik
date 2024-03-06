ff211ca6676a222992536f27b12dc8dbc18f57a4
Madhu <142790662+nmadhukarthik@users.noreply.github.com>
2024-03-06 18:28:37 +0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function onesDigit(n) {
   // your code here
+	if(n>0)
+	{
+		let ones = n%10;
+		return ones;
+	}
+	else
+	{
+		return "Enter positive number";
+	}
+	
 }
 
 // Do not change the code below
